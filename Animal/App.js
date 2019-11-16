41c71b60b3776104e2b72882745d53d0f5f9ef10
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2019-11-15 21:06:39 -0500

    Animal picker
    Lets user pick an animal for the game

--- Animal/App.js
+++ Animal/App.js
@@ -10,11 +10,13 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import MainTabNavigator from './navigation/MainTabNavigator';
 import QuestionOne from './screens/Question_One';
 import HomeScreen from './screens/HomeScreen';
+import AnimalPicker from './screens/Animal_Picker';
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    Picker: AnimalPicker,
     Main: MainTabNavigator,
     QOne: QuestionOne,
     Home: HomeScreen,
